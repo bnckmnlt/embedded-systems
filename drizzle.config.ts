6c7272c61617e885d73c018ae6f4bb0597b0113a
unknown <bryanmontealtomain@gmail.com>
2024-09-14 09:30:52 +0800

    Environment variables fixes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,12 +2,14 @@ import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
 import env from "@/lib/env";
 
+config({ path: ".env.local" });
+
 export default defineConfig({
   schema: "./src/lib/db/schema/index.ts",
   out: "./src/lib/db/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: env.DB_URL,
+    url: env.DB_URL || process.env.DB_URL_EXPANDED!,
   },
   strict: true,
   verbose: true,
--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -3,7 +3,11 @@ import { neon } from "@neondatabase/serverless";
 import * as schema from "@/lib/db/schema/index";
 import env from "@/lib/env";
 
-export const sql = neon(env.DB_URL);
+if (!process.env.DATABASE_URL) {
+  throw new Error("Database url not found");
+}
+
+export const sql = neon(env.DB_URL || process.env.DATABASE_URL!);
 export const db = drizzle(sql, { schema });
 
 export type db = typeof db;

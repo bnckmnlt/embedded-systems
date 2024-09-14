963e6d01137aa56b91fe57525ce043daef09dd88
unknown <bryanmontealtomain@gmail.com>
2024-09-14 09:35:04 +0800

    Environment variables fixes

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -3,11 +3,11 @@ import { neon } from "@neondatabase/serverless";
 import * as schema from "@/lib/db/schema/index";
 import env from "@/lib/env";
 
-if (!process.env.DATABASE_URL) {
+if (!process.env.DB_URL_EXPANDED) {
   throw new Error("Database url not found");
 }
 
-export const sql = neon(env.DB_URL || process.env.DATABASE_URL!);
+export const sql = neon(env.DB_URL || process.env.DB_URL_EXPANDED!);
 export const db = drizzle(sql, { schema });
 
 export type db = typeof db;

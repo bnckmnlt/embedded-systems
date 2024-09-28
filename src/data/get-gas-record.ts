f1ed00e381e719e7519538b75d301c103b0d0715
unknown <bryanmontealtomain@gmail.com>
2024-09-28 14:53:41 +0800

    fix: added route for gas

--- src/data/get-gas-record.ts
+++ src/data/get-gas-record.ts
@@ -2,7 +2,7 @@ import { fetchSingleGasRecord, Gas } from "$/server/actions/actions";
 import { useQuery } from "@tanstack/react-query";
 import Error from "next/error";
 
-export function useGetSingleGasRecord(): Promise<Gas, Error> {
+export function useGetSingleGasRecord() {
   return useQuery({
     queryKey: ["gas"],
     queryFn: async () => fetchSingleGasRecord(),
